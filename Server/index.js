effb431567090f781f3bf69675af2acc728c8e6c
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-09 12:29:03 +0530

    feat: add db connection

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello");
+});
+
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => console.log("DB connection successful"))
+	.catch((err) => console.log(`An error has occured ${err}`));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+	console.log(`Server is running at port ${PORT}`);
+});
