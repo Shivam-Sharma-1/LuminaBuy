d0d4df6f4265a03627fde15bd02b13d9f879af80
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-09 15:07:08 +0530

    feat: add cart schema model

---
+++ Server/models/Cart.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+	{
+		userId: { type: String, required: true },
+		products: [
+			{
+				productId: { type: String },
+				quantity: { type: Number, default: 0 }
+			}
+		]
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("cart", CartSchema);
