ef44889a44cf098943cbaa371a123c16c4d55d9d
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-09 15:09:59 +0530

    feat: add order schema model

---
+++ Server/models/Order.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose");
+
+const OrderSchema = new mongoose.Schema(
+	{
+		userId: { type: String, required: true },
+		products: [
+			{
+				productId: { type: String },
+				quantity: { type: Number, default: 0 }
+			}
+		],
+		amount: { type: Number, required: true },
+		address: { type: Object, required: true },
+		status: { type: String, default: "pending" }
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("order", OrderSchema);
