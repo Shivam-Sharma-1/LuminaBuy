318a78caa94b1d02c9b4a0fa474ad72dd2810631
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-09 21:26:09 +0530

    feat: add passord hasing

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,10 +1,17 @@
 const StatusCodes = require("http-status-codes");
+const CryptoJS = require("crypto-js");
 
 const User = require("../models/User");
 
 async function register(req, res) {
 	try {
-		const user = await User.create({ ...req.body });
+		const user = await User.create({
+			...req.body,
+			password: CryptoJS.AES.encrypt(
+				req.body.password,
+				process.env.PASSWORD_SECRET
+			)
+		});
 		res.status(StatusCodes.CREATED).json(user);
 	} catch (err) {
 		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
