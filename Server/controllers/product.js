5283597e874c357e244e52f601b3a2caca9e55d1
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-10 16:30:27 +0530

    feat: add all products route controllers

--- Server/controllers/product.js
+++ Server/controllers/product.js
@@ -24,6 +24,15 @@ async function getAllProducts(req, res) {
 	}
 }
 
+async function getProduct(req, res) {
+	try {
+		const product = await Product.findById(req.params.id);
+		res.status(StatusCodes.OK).json(product);
+	} catch (error) {
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+	}
+}
+
 async function createProduct(req, res) {
 	try {
 		const product = await Product.create(req.body);
@@ -33,4 +42,31 @@ async function createProduct(req, res) {
 	}
 }
 
-module.exports = { getAllProducts, createProduct };
+async function updateProduct(req, res) {
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{ new: true }
+		);
+		res.status(StatusCodes.CREATED).json(updatedProduct);
+	} catch (error) {
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+	}
+}
+
+async function deleteProduct(req, res) {
+	try {
+		await Product.findByIdAndDelete(req.params.id);
+		res.status(StatusCodes.OK).json("Product has been deleted");
+	} catch (error) {
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+	}
+}
+module.exports = {
+	getAllProducts,
+	getProduct,
+	createProduct,
+	updateProduct,
+	deleteProduct
+};
