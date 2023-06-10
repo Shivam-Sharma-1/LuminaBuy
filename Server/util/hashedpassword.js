624403a4faa53667168e293230f94ee94adc2bae
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-10 09:49:58 +0530

    feat: add decrypt function

--- Server/util/hashedpassword.js
+++ Server/util/hashedpassword.js
@@ -1,7 +1,16 @@
 const CryptoJS = require("crypto-js");
 
 function hashedPassword(password) {
-	return CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET);
+	return CryptoJS.AES.encrypt(
+		password,
+		process.env.PASSWORD_SECRET
+	).toString();
 }
 
-module.exports = hashedPassword;
+function decryptPassword(password) {
+	const bytes = CryptoJS.AES.decrypt(password, process.env.PASSWORD_SECRET);
+
+	return bytes.toString(CryptoJS.enc.Utf8);
+}
+
+module.exports = { hashedPassword, decryptPassword };
