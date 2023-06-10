9f3c130012fd2f1bf75cad9dedb3b103e276dbfe
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-10 13:29:51 +0530

    feat: add get all  users route

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -3,11 +3,12 @@ const {
 	authorizationMiddleware,
 	authorizeAdminMiddleware
 } = require("../middleware/authentication");
-const { editUser, deleteUser } = require("../controllers/user");
+const { updateUser, deleteUser, getAllUsers } = require("../controllers/user");
 const router = express.Router();
 
 router
-	.put("/:id", authorizationMiddleware, editUser)
+	.get("/", authorizeAdminMiddleware, getAllUsers)
+	.put("/:id", authorizationMiddleware, updateUser)
 	.delete("/:id", authorizeAdminMiddleware, deleteUser);
 
 module.exports = router;
