81683ae655a6015245b44fbf2e38640799fc7154
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-10 16:50:15 +0530

    feat: add all cart routes endpoints

---
+++ Server/routes/cart.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {
+	getAllItems,
+	createItem,
+	getItem,
+	updateItem,
+	deleteItem
+} = require("../controllers/cart");
+const {
+	authorizeAdminMiddleware,
+	authorizationMiddleware,
+	authenticationMiddleware
+} = require("../middleware/authentication");
+const router = express.Router();
+
+router
+	.get("/", authorizeAdminMiddleware, getAllItems)
+	.post("/", authenticationMiddleware, createItem)
+	.get("/:id", authorizationMiddleware, getItem)
+	.put("/:id", authorizationMiddleware, updateItem)
+	.delete("/:id", authorizationMiddleware, deleteItem);
+
+module.exports = router;
