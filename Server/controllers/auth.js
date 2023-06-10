0f90e8aa1272c48325a6bb4cc4aa0c40c1820061
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-10 10:00:48 +0530

    feat: add login controller

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,21 +1,36 @@
 const StatusCodes = require("http-status-codes");
-const CryptoJS = require("crypto-js");
 
 const User = require("../models/User");
+const { hashedPassword, decryptPassword } = require("../util/hashedpassword");
 
 async function register(req, res) {
 	try {
 		const user = await User.create({
 			...req.body,
-			password: CryptoJS.AES.encrypt(
-				req.body.password,
-				process.env.PASSWORD_SECRET
-			)
+			password: hashedPassword(req.body.password)
 		});
 		res.status(StatusCodes.CREATED).json(user);
-	} catch (err) {
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+	} catch (error) {
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
 	}
 }
 
-module.exports = { register };
+async function login(req, res) {
+	try {
+		const user = await User.findOne({
+			email: req.body.email
+		});
+
+		!user && res.status(StatusCodes.UNAUTHORIZED).json("Wrong credentials");
+		req.body.password != decryptPassword(user.password) &&
+			res.status(StatusCodes.UNAUTHORIZED).json("Wrong password");
+
+		const { password, ...userDetails } = user._doc;
+
+		res.status(StatusCodes.OK).json(userDetails);
+	} catch (error) {
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+	}
+}
+
+module.exports = { register, login };
