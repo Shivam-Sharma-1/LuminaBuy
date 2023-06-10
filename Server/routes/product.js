5d3869d45f5864a8e5c7c2447730c55ecde447b8
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-10 16:30:54 +0530

    feat: add all products endpoints

--- Server/routes/product.js
+++ Server/routes/product.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/product");
+const {
+	getAllProducts,
+	createProduct,
+	getProduct,
+	updateProduct,
+	deleteProduct
+} = require("../controllers/product");
 const { authorizeAdminMiddleware } = require("../middleware/authentication");
 const router = express.Router();
 
 router
 	.get("/", getAllProducts)
-	.post("/", authorizeAdminMiddleware, createProduct);
+	.post("/", authorizeAdminMiddleware, createProduct)
+	.get("/:id", getProduct)
+	.put("/:id", authorizeAdminMiddleware, updateProduct)
+	.delete("/:id", authorizeAdminMiddleware, deleteProduct);
 
 module.exports = router;
