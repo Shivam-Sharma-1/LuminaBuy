e49fb9c02c36a6542826b3fea5e74756572df71a
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-10 18:29:12 +0530

    feat: use cors and stripe

--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,8 @@ const usersRouter = require("./routes/user");
 const productsRouter = require("./routes/product");
 const cartsRouter = require("./routes/cart");
 const ordersRouter = require("./routes/order");
+const stripeRouter = require("./routes/stripe");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
@@ -13,17 +15,19 @@ app.get("/", (req, res) => {
 	res.send("Hello");
 });
 
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => console.log("DB connection successful"))
+	.catch((err) => console.log(`An error has occured ${err}`));
+
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/cart", cartsRouter);
 app.use("/api/orders", ordersRouter);
-
-mongoose
-	.connect(process.env.MONGODB_URI)
-	.then(() => console.log("DB connection successful"))
-	.catch((err) => console.log(`An error has occured ${err}`));
+app.use("/api/checkout", stripeRouter);
 
 const PORT = process.env.PORT || 5000;
 
