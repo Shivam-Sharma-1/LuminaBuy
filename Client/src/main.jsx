c53b03cd4cbfc3bb2d7b70e12a5f7b4bcb1fb80c
Shivam77607 <shivamsharma77607@gmail.com>
2023-09-16 16:29:38 +0530

    feat: add vercel analytics

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -9,6 +10,7 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
+			<Analytics />
 			<PersistGate loading={null} persistor={persistor}>
 				<App />
 			</PersistGate>
